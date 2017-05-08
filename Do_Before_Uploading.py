b988307094ad1da1dddb456b77368c3da5019e13
Guofei <guofei9987@foxmail.com>
2017-05-08 22:03:44 +0800

    AutoUpload

--- Do_Before_Uploading.py
+++ Do_Before_Uploading.py
@@ -14,15 +14,17 @@ ind=ind-33
 #读取excel
 import xlrd
 workbook=xlrd.open_workbook("2017.xlsx")
-sheet1=workbook.sheet_by_name("2月")
-data=sheet1.cell(3,5).value
+sheet1=workbook.sheet_by_name("12月")
+data1=sheet1.cell(3,5).value#未完成的总任务工期
+data2=sheet1.cell(3,8).value#总任务工期
+
 
 #写入
 import win32com.client
 xlApp = win32com.client.Dispatch('Excel.Application') #打开EXCEL，这里不需改动
 xlBook = xlApp.Workbooks.Open("C:\\Users\\guofei\\Desktop\\gitH\\plans\\2017.xlsx")
 xlSht2=xlBook.Worksheets("列表")
-xlSht2.Cells(ind,2).Value = data #可以用这种方法给指定的单元格赋值
+xlSht2.Cells(ind,2).Value = data2-data1-ind #可以用这种方法给指定的单元格赋值
 xlBook.Close(SaveChanges=1) #完成 关闭保存文件
 del xlApp
 
